9f928a4220f0bf2f9ecec373f132724008e857ce
TWBMT <idnattts@gmail.com>
2022-09-24 00:54:31 +0900

    add mocked Minesweeper

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { MinesweeperInterface } from './minesweeper/minesweeper';
+import { MinesweeperInterface } from './minesweeper/interfaces/minesweeper';
+import { MinesweeperEndEvent } from './minesweeper/interfaces/end-events';
+import { SubmitMinesweeperEvents } from './minesweeper/interfaces/start-events';
+
+const mockedMinesweeper: MinesweeperInterface = {
+  addEndingEventListner: function (event: MinesweeperEndEvent): void {
+    throw new Error('Function not implemented.');
+  },
+  submitUiEvent: function (event: SubmitMinesweeperEvents): void {
+    throw new Error('Function not implemented.');
+  }
+}
+
 
 function App() {
-  // TODO
-  const instance: MinesweeperInterface = {} as any;
+  const instance: MinesweeperInterface = mockedMinesweeper;
   return (
     <div className="App">
-      <header className="App-header" onClick={instance.clickCell()}>
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
